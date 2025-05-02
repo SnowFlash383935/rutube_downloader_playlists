8d66e40629cf9664c5c07002f970c775d76893f8
SnowFlash383935 <filipplucs@gmail.com>
2025-05-02 16:44:57 +0300

    Create playlists.py

---
+++ src/playlists.py
@@ -0,0 +1,22 @@
+import requests
+from rutube import Rutube
+from urllib import parse
+
+class RutubeRealPlaylist:
+    def __init__(self, url):
+        try:
+            id = int(parse.urlparse(url).path[1:-1].split("/")[1])
+        except:
+            raise ValueError(f"Invalid URL: {url}!")
+        print(id)
+        page = 0
+        result = requests.get(f"https://rutube.ru/api/playlist/custom/{id}/videos/?client=wdp&page={page}").json()
+        self.all_raw = result["results"]
+        while result["has_next"]:
+            page += 1
+            result = requests.get(f"https://rutube.ru/api/playlist/custom/{id}/videos/?client=wdp&page={page}").json()
+            self.all_raw += result["results"]
+    def __len__(self):
+        return len(self.all_raw)
+    def __getitem__(self, itemnum):
+        return Rutube(self.all_raw[itemnum]["video_url"])
